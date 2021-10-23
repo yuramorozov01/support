1e1d7a7102a740f32f1a09630eb37f5dffc11f85
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-24 00:08:37 +0300

    feat: update viewset with handler of getting list of user's tickets (SP-7)

--- support/src/support_app/views.py
+++ support/src/support_app/views.py
@@ -1,6 +1,6 @@
 from rest_framework import permissions, viewsets
 from .models import Message, Ticket
-from .serializers import TicketCreateSerializer, TicketDetailsSerializer
+from .serializers import TicketCreateSerializer, TicketDetailsSerializer, TicketListSerializer
 
 
 class TicketViewSet(viewsets.ModelViewSet):
@@ -10,16 +10,24 @@ class TicketViewSet(viewsets.ModelViewSet):
 
     retrieve:
         Return the specified ticket.
+
+    list:
+        Return a list of all user's tickets.
+
     '''
 
     permission_classes = [permissions.IsAuthenticated]
 
     def get_queryset(self):
         if self.action == 'retrieve':
-            return Ticket.objects.all()
+            return Ticket.objects.all().filter(author=self.request.user)
+        elif self.action == 'list':
+            return Ticket.objects.all().filter(author=self.request.user)
 
     def get_serializer_class(self):
         if self.action == 'create':
             return TicketCreateSerializer
         elif self.action == 'retrieve':
             return TicketDetailsSerializer
+        elif self.action == 'list':
+            return TicketListSerializer
