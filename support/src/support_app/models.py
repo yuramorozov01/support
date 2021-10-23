d257406f54066f5a9757cb2f78467fed7c9b4f5c
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-23 18:53:40 +0300

    feat: add ticket model (SP-3)

--- support/src/support_app/models.py
+++ support/src/support_app/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Ticket(models.Model):
+    '''Ticket Model
+    User can send a ticket with question to a support user.
+    Ticket status can be changed only by a support user.
+    '''
+
+    OPEN_STATUS = 1
+    CLOSED_STATUS = 2
+    FREEZED_STATUS = 3
+    STATUS_CHOICES = (
+        (OPEN_STATUS, 'Open'),
+        (CLOSED_STATUS, 'Closed'),
+        (FREEZED_STATUS, 'Freezed'),
+    )
+
+    title = models.CharField('Title', max_length=128)
+    status = models.IntegerField(choices=STATUS_CHOICES, default=OPEN_STATUS)
+    text = models.TextField('Question', max_length=8192)
+    author = models.ForeignKey(User, verbose_name='Author', on_delete=models.CASCADE, related_name='tickets')
+    created_at = models.DateTimeField('Creation time', auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Ticket'
+        verbose_name_plural = 'Tickets'
+        
