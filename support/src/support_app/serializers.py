4a239ad01d8a7502a350ffaaa5a84a012fdce721
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-24 18:25:40 +0300

    feat: add serializer to update ticket (SP-10)

--- support/src/support_app/serializers.py
+++ support/src/support_app/serializers.py
@@ -3,6 +3,14 @@ from .models import Message, Ticket
 from rest_framework import serializers
 
 
+class CustomUserSerializer(serializers.ModelSerializer):
+    '''Serializer for an user'''
+
+    class Meta:
+        model = User
+        fields = ['id', 'username', 'email']
+
+
 class TicketCreateSerializer(serializers.ModelSerializer):
     '''Serializer for creating ticket'''
 
@@ -12,12 +20,20 @@ class TicketCreateSerializer(serializers.ModelSerializer):
         extra_kwargs = {'author': {'default': serializers.CurrentUserDefault()}}
 
 
-class CustomUserSerializer(serializers.ModelSerializer):
-    '''Serializer for an user'''
+class TicketUpdateSerializer(serializers.ModelSerializer):
+    '''Serializer for updating ticket'''
+
+    author = CustomUserSerializer(read_only=True)
 
     class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
+        model = Ticket
+        fields = '__all__'
+        extra_kwargs = {'author': {'default': serializers.CurrentUserDefault()}}
+
+    def validate_status(self, value):
+        if self.instance and value != self.instance.status:
+            raise serializers.ValidationError('Status can be changed only by a support')
+        return value
 
 
 class TicketDetailsSerializer(serializers.ModelSerializer):
