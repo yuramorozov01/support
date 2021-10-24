225bce5820e7e363b418188f826711f4dbb81476
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-24 18:26:00 +0300

    feat: add api url path to update ticket (SP-10)

--- support/src/support_app/urls.py
+++ support/src/support_app/urls.py
@@ -18,5 +18,5 @@ from . import views
 
 urlpatterns = [
     path('ticket/', views.TicketViewSet.as_view({'get': 'list', 'post': 'create'})),
-    path('ticket/<int:pk>/', views.TicketViewSet.as_view({'get': 'retrieve', 'delete': 'destroy'})),
+    path('ticket/<int:pk>/', views.TicketViewSet.as_view({'get': 'retrieve', 'delete': 'destroy', 'put': 'update', 'patch': 'partial_update'})),
 ]
