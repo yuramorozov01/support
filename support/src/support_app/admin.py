8da4068c25db1e1dfa5b7745e748d7634d4c16cb
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-26 19:14:41 +0300

    feat: add creating support group (SP-12)

--- support/src/support_app/admin.py
+++ support/src/support_app/admin.py
@@ -1,19 +1,19 @@
 from django.contrib import admin
-from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group, Permission
 
-from django.contrib.auth.models import Permission
+# Register your models here.
 
-# def add_group_permissions():
-#     # Support group
-#     group, created = Group.objects.get_or_create(name='support_user') 
-#     if created:
-#         can_view_all_tickets = Permission.objects.get(codename='can_view_all_tickets')
-#         group.permissions.add(can_view_all_tickets)
+def add_group_permissions():
+    # Support group
+    group, created = Group.objects.get_or_create(name='support_user') 
+    if created:
+        can_view_all_tickets = Permission.objects.get(codename='can_view_all_tickets')
+        group.permissions.add(can_view_all_tickets)
 
-#         can_change_status = Permission.objects.get(codename='can_change_status')
-#         group.permissions.add(can_change_status)
+        can_change_status = Permission.objects.get(codename='can_change_status')
+        group.permissions.add(can_change_status)
 
-#         can_message_in_different_tickets = Permission.objects.get(codename='can_message_in_different_tickets')
-#         group.permissions.add(can_message_in_different_tickets)
+        can_message_in_different_tickets = Permission.objects.get(codename='can_message_in_different_tickets')
+        group.permissions.add(can_message_in_different_tickets)
 
-#         logger.info('Support group has been created successfully!')
+add_group_permissions()
