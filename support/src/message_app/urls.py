abfd18be27c2476130169119f1178f70e8a6ab3b
yuramorozov01 <yura_morozov01@mail.ru>
2021-10-28 22:49:44 +0300

    refactor: isort refactoring

--- support/src/message_app/urls.py
+++ support/src/message_app/urls.py
@@ -1,6 +1,6 @@
-from django.urls import path, include
-from rest_framework.routers import DefaultRouter
+from django.urls import include, path
 from message_app import views
+from rest_framework.routers import DefaultRouter
 
 router = DefaultRouter()
 router.register(r'message', views.MessageViewSet, basename='Message')
